86df1ae75c2dac1aad4ed12c6837cb4a4ace2929
Nick Gauthier <nick@smartlogicsolutions.com>
2010-10-26 17:58:35 -0400

    basic test pilot dsl

--- test/integration/blog_test.rb
+++ test/integration/blog_test.rb
@@ -6,15 +6,6 @@ class BlogTest < ActionDispatch::IntegrationTest
     assert_see "Blog Posts"
   end
 
-  test "See some blog posts" do
-    b1 = Blog.create!(:title => "My post", :body => "This is my new blog post")
-    b2 = Blog.create!(:title => "My second post", :body => "This is my second blog post")
-    visit root_path
-    assert_see "Blog Posts"
-    assert_see_blog b1
-    assert_see_blog b2
-  end
-
   test "Create a new post" do
     visit root_path
     fill_in "Title", :with => "a blog title"
@@ -23,5 +14,16 @@ class BlogTest < ActionDispatch::IntegrationTest
     assert_on root_path
     assert_see 'a blog title'
     assert_see 'a blog body'
+  end
+
+  test "See some blog posts - Pilot" do
+    b1 = Blog.create!(:title => "My post", :body => "This is my new blog post")
+    b2 = Blog.create!(:title => "My second post", :body => "This is my second blog post")
+
+    BlogPilot do
+      assert_see_blog(b1)
+      assert_see_blog(b2)
+    end
+
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,19 +5,48 @@ require 'capybara'
 require 'capybara/dsl'
 Capybara.app = TestPilotDemo::Application
 
+module TestPilot
+  class Core < ActionDispatch::IntegrationTest
+    def initialize
+    end
+
+    def self.inherited(subclass)
+      puts "Inherited by #{subclass}"
+      TestPilot::Dsl.send(:define_method, subclass.to_s) do |&block|
+        subclass.new.instance_eval(&block)
+      end
+    end
+  end
+
+  module Dsl
+  end
+end
+
+class BlogPilot < TestPilot::Core
+  def assert_see_blog(blog)
+    visit(root_path)
+    assert_see "Blog Posts"
+    within("#blogs") { assert_see(blog.title) }
+    within("#blogs") { assert_see(blog.body) }
+  end
+end
+
+  #def BlogPilot(&block)
+  #  BlogPilot.new(block.binding).instance_eval{block.call}
+  #end
+
 class ActiveSupport::TestCase
   include Capybara
+  include TestPilot::Dsl
 
   def assert_see(content)
     assert(page.has_content?(content), "Expected page to contain \"#{content}\", but it didn't")
   end
 
-  def assert_see_blog(blog)
-    within("#blogs") { assert_see(blog.title) }
-    within("#blogs") { assert_see(blog.body) }
-  end
 
   def assert_on(path)
     assert_equal(path, current_path)
   end
+
 end
+
