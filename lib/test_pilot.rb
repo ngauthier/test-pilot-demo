a046d58565e757fea6f92850fc3f6c7fbda5e3c9
Nick Gauthier <nick@smartlogicsolutions.com>
2010-10-26 21:55:01 -0400

    reduce test::pilot

--- lib/test_pilot.rb
+++ lib/test_pilot.rb
@@ -1,18 +1,13 @@
 module TestPilot
   class Core < ActionDispatch::IntegrationTest
-    def initialize
-    end
-
+    def initialize ; end # override new from inherited class
     def self.inherited(subclass)
-      puts "Inherited by #{subclass}"
       TestPilot::Dsl.send(:define_method, subclass.to_s) do |&block|
         subclass.new.instance_eval(&block)
       end
     end
   end
-
-  module Dsl
-  end
+  module Dsl ; end
 end
 
 
